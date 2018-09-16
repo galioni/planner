eb4cf9fca8b37674f040635696a6d786d4c6287e
Mateusz Rogoś <mat.rogos@gmail.com>
2018-09-16 11:45:27 +0100

    Initial commit with projects and sample class with repository implementation

---
+++ Planner/Planner.Core/Domain/Project.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planner.Core.Domain
+{
+    public class Project
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Project(string name, string description)
+        {       
+            SetName(name);
+            SetDescription(description);
+            this.ID = new Guid();
+            this.CreatedAt = DateTime.Now;
+        }
+
+        private void SetDescription(string description)
+        {
+            //If needed we can add here validation for the field
+            this.Description = description;
+        }
+
+        private void SetName(string name)
+        {
+            if (String.IsNullOrEmpty(name)) throw new ArgumentException("Project's name can not be empty");
+
+            this.Name = name;
+        }
+
+        private void SetUpdate()
+        {
+            this.UpdatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ Planner/Planner.Infrastructure/Data/PlannerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Planner.Core.Domain;
+
+namespace Planner.Infrastructure.Data
+{
+    class PlannerContext : DbContext
+    {
+        public PlannerContext(DbContextOptions<PlannerContext> options) : base(options) { }
+
+        public PlannerContext() { }
+
+        public DbSet<Project> Projects {get;set;}  
+    }
+}
