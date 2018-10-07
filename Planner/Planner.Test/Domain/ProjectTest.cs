3f4ef5a6cbc7cf9dcb71d711b82d2afe7b4c7f54
matro <mat.rogos@gmail.com>
2018-10-07 18:15:59 +0100

    Removed blank unit test class and created tests for Project class

---
+++ Planner/Planner.Test/Domain/ProjectTest.cs
@@ -0,0 +1,74 @@
+﻿using Planner.Core.Domain;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Planner.Test.Domain
+{
+    public class ProjectTest
+    {
+        public Project GetProject()
+        {
+            return new Project("Test name", "Test description");
+        }
+        [Fact]
+        public void CTOR_AllParametersPopulated_objectCreated()
+        {
+            Project project = new Project("Test name", "Test description");
+            Assert.Equal("Test name", project.Name);
+            Assert.Equal("Test description", project.Description);
+            Assert.NotEqual(new DateTime(), project.CreatedAt);
+        }
+
+        [Fact]
+        public void CTOR_MissingDescription_objectCreated()
+        {
+            Project project = new Project("Test name", "");
+            Assert.Equal("Test name", project.Name);
+            Assert.Equal(String.Empty, project.Description);
+            Assert.NotEqual(new DateTime(), project.CreatedAt);
+        }
+
+        [Fact]
+        public void CTOR_MissingName_objectNotCreated_ExpcetionThrown()
+        {
+            Action act = () => new Project("", "");
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void SetName_UpdateName()
+        {
+            var project = GetProject();
+            string newName = "New name";
+            project.SetName(newName);
+            Assert.Equal(newName, project.Name);
+        }
+
+        [Fact]
+        public void SetName_MissingName_objectNotCreatedExpcetionThrown()
+        {
+            var project = GetProject();
+            Action act = () => project.SetName("");
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void SetDescription_UpdateDescription()
+        {
+            var project = GetProject();
+            string newDescription = "New description";
+            project.SetDescription(newDescription);
+            Assert.Equal(newDescription, project.Description);
+        }
+
+        [Fact]
+        public void SetDescription_BlankNewValu_UpdateDescriptionDontThrowException()
+        {
+            var project = GetProject();
+            string newDescription = "";
+            project.SetDescription(newDescription);
+            Assert.Equal(newDescription, project.Description);
+        }
+    }
+}
